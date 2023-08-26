7df7fde8c31c12a783485487769c473929333e7f
Ivanek83 <ivanpablok@hotmail.com>
2023-08-26 13:51:10 +0000

    actualizo

---
+++ src/rpart/141_gridsearch_esqueleto_001_optimizacion 2.r
@@ -0,0 +1,165 @@
+# esqueleto de grid search
+# se espera que los alumnos completen lo que falta
+#   para recorrer TODOS cuatro los hiperparametros
+
+rm(list = ls()) # Borro todos los objetos
+gc() # Garbage Collection
+
+require("data.table")
+require("rpart")
+require("parallel")
+
+PARAM <- list()
+# reemplazar por las propias semillas
+PARAM$semillas <- c(124541, 203663, 365567, 449437, 565057)
+
+#------------------------------------------------------------------------------
+# particionar agrega una columna llamada fold a un dataset
+#  que consiste en una particion estratificada segun agrupa
+# particionar( data=dataset, division=c(70,30), agrupa=clase_ternaria, seed=semilla)
+#   crea una particion 70, 30
+
+particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, seed = NA) {
+  if (!is.na(seed)) set.seed(seed)
+
+  bloque <- unlist(mapply(function(x, y) {
+    rep(y, x)
+  }, division, seq(from = start, length.out = length(division))))
+
+  data[, (campo) := sample(rep(bloque, ceiling(.N / length(bloque))))[1:.N], # nolint
+    by = agrupa
+  ]
+}
+#------------------------------------------------------------------------------
+
+ArbolEstimarGanancia <- function(semilla, param_basicos) { # nolint
+  # particiono estratificadamente el dataset
+  particionar(dataset, division = c(7, 3), agrupa = "clase_ternaria", seed = semilla) # nolint: line_length_linter.
+
+  # genero el modelo
+  # quiero predecir clase_ternaria a partir del resto
+  modelo <- rpart("clase_ternaria ~ .",
+    data = dataset[fold == 1], # fold==1  es training,  el 70% de los datos
+    xval = 0,
+    control = param_basicos
+  ) # aqui van los parametros del arbol
+
+  # aplico el modelo a los datos de testing
+  prediccion <- predict(modelo, # el modelo que genere recien
+    dataset[fold == 2], # fold==2  es testing, el 30% de los datos
+    type = "prob"
+  ) # type= "prob"  es que devuelva la probabilidad
+
+  # prediccion es una matriz con TRES columnas,
+  #  llamadas "BAJA+1", "BAJA+2"  y "CONTINUA"
+  # cada columna es el vector de probabilidades
+
+
+  # calculo la ganancia en testing  qu es fold==2
+  ganancia_test <- dataset[
+    fold == 2,
+    sum(ifelse(prediccion[, "BAJA+2"] > 0.025,
+      ifelse(clase_ternaria == "BAJA+2", 117000, -3000),
+      0
+    ))
+  ]
+
+  # escalo la ganancia como si fuera todo el dataset
+  ganancia_test_normalizada <- ganancia_test / 0.3
+
+  return(ganancia_test_normalizada)
+}
+#------------------------------------------------------------------------------
+
+ArbolesMontecarlo <- function(semillas, param_basicos) { # nolint: object_name_linter.
+  # la funcion mcmapply  llama a la funcion ArbolEstimarGanancia
+  #  tantas veces como valores tenga el vector  ksemillas
+  ganancias <- mcmapply(ArbolEstimarGanancia,
+    semillas, # paso el vector de semillas
+    MoreArgs = list(param_basicos), # aqui paso el segundo parametro
+    SIMPLIFY = FALSE,
+    mc.cores = 1
+  ) # se puede subir a 5 si posee Linux o Mac OS
+
+  ganancia_promedio <- mean(unlist(ganancias))
+
+  return(ganancia_promedio)
+}
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+# Aqui se debe poner la carpeta de la computadora local
+setwd("~/buckets/b1/") # Establezco el Working Directory
+# cargo los datos
+
+# cargo los datos
+dataset <- fread("./datasets/dataset_pequeno.csv")
+
+# trabajo solo con los datos con clase, es decir 202107
+dataset <- dataset[clase_ternaria != ""]
+
+# genero el archivo para Kaggle
+# creo la carpeta donde va el experimento
+# HT  representa  Hiperparameter Tuning
+dir.create("./exp/", showWarnings = FALSE)
+dir.create("./exp/HT2020/", showWarnings = FALSE)
+archivo_salida <- "./exp/HT2020/gridsearch_optimizado_2.txt"
+
+# Escribo los titulos al archivo donde van a quedar los resultados
+# atencion que si ya existe el archivo, esta instruccion LO SOBREESCRIBE,
+#  y lo que estaba antes se pierde
+# la forma que no suceda lo anterior es con append=TRUE
+cat(
+  file = archivo_salida,
+  sep = "",
+  "max_depth", "\t",
+  "min_split", "\t",
+  "min_bucket", "\t",
+  "cp", "\t",
+  "ganancia_promedio", "\n"
+)
+
+
+# itero por los loops anidados para cada hiperparametro
+
+
+for (vmax_depth in c(9, 10, 11, 12, 13)) {
+  for (vmin_split in c(100, 200, 300, 400, 500)) {
+    for (min_bucket in c(50, 100, 150, 200, 300, 400, 1000)) {
+      for (cp in c(-0.3)) {
+        if(vmin_split >= 2*min_bucket) {
+          
+          print("Iteracion")
+          print(vmax_depth)
+          print(vmin_split)
+          print(min_bucket)
+          print(cp)
+          cat("\n")
+
+          
+          param_basicos <- list(
+            "cp" = cp,
+            "minsplit" = vmin_split,
+            "minbucket" = min_bucket,
+            "maxdepth" = vmax_depth
+          )
+
+          # Un solo llamado, con la semilla 17
+          ganancia_promedio <- ArbolesMontecarlo(PARAM$semillas, param_basicos)
+
+          # escribo los resultados al archivo de salida
+          cat(
+            file = archivo_salida,
+            append = TRUE,
+            sep = "",
+            vmax_depth, "\t",
+            vmin_split, "\t",
+            min_bucket, "\t",
+            cp, "\t",
+            ganancia_promedio, "\n"
+          )
+        }
+      }
+    }
+  }
+}
