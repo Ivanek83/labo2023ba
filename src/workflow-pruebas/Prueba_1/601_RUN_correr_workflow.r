81f6302dd662eac0ddb130a80523a1cad905bd51
Ivan <ivanpablok@hotmail.com>
2023-09-23 01:02:46 -0300

    .

--- src/workflow-pruebas/Prueba_1/601_RUN_correr_workflow.r
+++ src/workflow-pruebas/Prueba_1/601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-pruebas/611_CA_reparar_dataset_001.r")
-source("~/labo2023ba/src/workflow-pruebas/621_DR_corregir_drifting_001.r")
-source("~/labo2023ba/src/workflow-pruebas/631_FE_historia_001.r")
-source("~/labo2023ba/src/workflow-pruebas/641_TS_training_strategy_001.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_1/611_CA_reparar_dataset_001.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_1/621_DR_corregir_drifting_001.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_1/631_FE_historia_001.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_1/641_TS_training_strategy_001.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-pruebas/651_HT_lightgbm_001.r")
-source("~/labo2023ba/src/workflow-pruebas/661_ZZ_final_001.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_1/651_HT_lightgbm_001.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_1/661_ZZ_final_001.r")
--- src/workflow-pruebas/Prueba_2/601_RUN_correr_workflow.r
+++ src/workflow-pruebas/Prueba_2/601_RUN_correr_workflow.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-pruebas/611_CA_reparar_dataset_002.r")
-source("~/labo2023ba/src/workflow-pruebas/621_DR_corregir_drifting_002.r")
-source("~/labo2023ba/src/workflow-pruebas/631_FE_historia_002.r")
-source("~/labo2023ba/src/workflow-pruebas/641_TS_training_strategy_002.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_2/611_CA_reparar_dataset_002.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_2/621_DR_corregir_drifting_002.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_2/631_FE_historia_002.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_2/641_TS_training_strategy_002.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-pruebas/651_HT_lightgbm_002.r")
-source("~/labo2023ba/src/workflow-pruebas/661_ZZ_final_002.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_2/651_HT_lightgbm_002.r")
+source("~/labo2023ba/src/workflow-pruebas/Prueba_2/661_ZZ_final_002.r")
