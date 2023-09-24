01e271d2d1cf22776257664bed019b85cfacef6e
Ivan <ivanpablok@hotmail.com>
2023-09-24 12:05:17 -0300

    c

--- src/workflow-pruebas/Prueba_3/601_RUN_correr_workflow.r
+++ src/workflow-pruebas/Prueba_3/601_RUN_correr_workflow.r
@@ -11,23 +11,23 @@ options(error = function() {
 
 # primeros pasos, relativamente rapidos
 
-cat("Inicia prueba 003: 611 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
+cat("\n\n Inicia prueba 003: 611 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/611_CA_reparar_dataset_003.r")
 
-cat("Inicia prueba 003: 621 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
+cat("\n\n Inicia prueba 003: 621 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/621_DR_corregir_drifting_003.r")
 
-cat("Inicia prueba 003: 631 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
+cat("\n\n Inicia prueba 003: 631 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/631_FE_historia_003.r")
 
-cat("Inicia prueba 003: 641 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
+cat("\n\n Inicia prueba 003: 641 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/641_TS_training_strategy_003.r")
 
 
 # ultimos pasos, muy lentos
 
-cat("Inicia prueba 003: 651 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
+cat("\n\n Inicia prueba 003: 651 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/651_HT_lightgbm_003.r")
 
-cat("Inicia prueba 003: 661 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
+cat("\n\n Inicia prueba 003: 661 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/661_ZZ_final_003.r")
