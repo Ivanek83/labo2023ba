09a04995690ca47c373c44f44858f5d49fd791bb
Ivan <ivanpablok@hotmail.com>
2023-09-24 11:56:30 -0300

    hora

--- src/workflow-pruebas/Prueba_3/601_RUN_correr_workflow.r
+++ src/workflow-pruebas/Prueba_3/601_RUN_correr_workflow.r
@@ -10,17 +10,24 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-cat("Inicia prueba 003: 611 \n")
+
+cat("Inicia prueba 003: 611 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/611_CA_reparar_dataset_003.r")
-cat("Inicia prueba 003: 621 \n")
+
+cat("Inicia prueba 003: 621 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/621_DR_corregir_drifting_003.r")
-cat("Inicia prueba 003: 631 \n")
+
+cat("Inicia prueba 003: 631 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/631_FE_historia_003.r")
-cat("Inicia prueba 003: 641 \n")
+
+cat("Inicia prueba 003: 641 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/641_TS_training_strategy_003.r")
 
+
 # ultimos pasos, muy lentos
-cat("Inicia prueba 003: 651 \n")
+
+cat("Inicia prueba 003: 651 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/651_HT_lightgbm_003.r")
-cat("Inicia prueba 003: 661 \n")
+
+cat("Inicia prueba 003: 661 - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n")
 source("~/labo2023ba/src/workflow-pruebas/Prueba_3/661_ZZ_final_003.r")
