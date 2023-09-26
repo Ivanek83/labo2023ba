61223cfecedaa0912a32992fb76a75438acc1dd9
Ivan <ivanpablok@hotmail.com>
2023-09-25 22:23:52 -0300

    .

--- src/workflow-pruebas/Prueba_4/601_RUN_correr_workflow_004.r
+++ src/workflow-pruebas/Prueba_4/601_RUN_correr_workflow_004.r
@@ -17,39 +17,39 @@ setwd("~/buckets/b1/exp/p04_RUN6010/")
 
 
 # corrida de cada paso del workflow
-
+archivo <- "Tiempo_Script_004.txt"
 # Registra la hora de inicio
 hora_inicio <- Sys.time()
 
-cat("\nInicia Script 004\n\n", file = "Tiempo_Script.txt", append = TRUE)
-cat("hora_inicio GMT0: ", hora_inicio, "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("Inicia workflow-pruebas -> Prueba_4\n", file = archivo, append = TRUE)
+cat("hora_inicio GMT: ", hora_inicio, "\n", file = archivo, append = TRUE)
 
 # primeros pasos, relativamente rapidos
 
-cat("\n\nScript 611_CA - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nScript 611_CA - Hora de inicio GMT: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = archivo, append = TRUE)
 source("~/labo2023ba/src/workflow-pruebas/Prueba_4/611_CA_reparar_dataset_004.r")
 
 setwd("~/buckets/b1/exp/p04_RUN6010/")
-cat("\n\nScript 621_DR - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nScript 621_DR - Hora de inicio GMT: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = archivo, append = TRUE)
 source("~/labo2023ba/src/workflow-pruebas/Prueba_4/621_DR_corregir_drifting_004.r")
 
 setwd("~/buckets/b1/exp/p04_RUN6010/")
-cat("\n\nScript 631_FE - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nScript 631_FE - Hora de inicio GMT: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = archivo, append = TRUE)
 source("~/labo2023ba/src/workflow-pruebas/Prueba_4/631_FE_historia_004.r")
 
 setwd("~/buckets/b1/exp/p04_RUN6010/")
-cat("\n\nScript 641_TS - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nScript 641_TS - Hora de inicio GMT: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = archivo, append = TRUE)
 source("~/labo2023ba/src/workflow-pruebas/Prueba_4/641_TS_training_strategy_004.r")
 
 
 # ultimos pasos, muy lentos
 
 setwd("~/buckets/b1/exp/p04_RUN6010/")
-cat("\n\nScript: 651_HT - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nScript: 651_HT - Hora de inicio GMT: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = archivo, append = TRUE)
 source("~/labo2023ba/src/workflow-pruebas/Prueba_4/651_HT_lightgbm_004.r")
 
 setwd("~/buckets/b1/exp/p04_RUN6010/")
-cat("\n\nScript 661_ZZ - Hora de inicio: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nScript 661_ZZ - Hora de inicio GMT: ", strftime(Sys.time(), format = "%Y-%m-%d %H:%M:%S"), "\n", file = archivo, append = TRUE)
 source("~/labo2023ba/src/workflow-pruebas/Prueba_4/661_ZZ_final_004.r")
 
 
@@ -59,7 +59,7 @@ source("~/labo2023ba/src/workflow-pruebas/Prueba_4/661_ZZ_final_004.r")
 setwd("~/buckets/b1/exp/p04_RUN6010/")
 # Registra la hora de finalización
 hora_fin <- Sys.time()
-cat("\n\nhora_fin GMT0: ", hora_fin, "\n", file = "Tiempo_Script.txt", append = TRUE)
+cat("\n\nhora_fin GMT: ", hora_fin, "\n", file = archivo, append = TRUE)
 
 # Calcula la diferencia de tiempo en segundos
 diferencia_segundos <- as.numeric(difftime(hora_fin, hora_inicio, units = "secs"))
@@ -71,6 +71,4 @@ minutos <- floor((diferencia_segundos %% 3600) / 60)
 segundos <- diferencia_segundos %% 60
 
 # Muestra el tiempo transcurrido
-cat("\n\nTiempo transcurrido Script 004:", dias, "días,", horas, "horas,", minutos, "minutos,", segundos, "segundos\n", file = "Tiempo_Script.txt", append = TRUE)
-
-
+cat("\n\nTiempo transcurrido workflow-pruebas -> Prueba_4:", dias, "días,", horas, "horas,", minutos, "minutos,", segundos, "segundos\n", file = archivo, append = TRUE)
